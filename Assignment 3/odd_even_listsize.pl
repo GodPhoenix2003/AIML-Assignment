041eff7ec8019ceaa26d239364d436f5e6fa8552
Sagnik Chatterjee <100948753+GodPhoenix2003@users.noreply.github.com>
2023-02-23 01:10:43 +0530

    Update odd_even_listsize.pl

--- Assignment 3/odd_even_listsize.pl
+++ Assignment 3/odd_even_listsize.pl
@@ -5,3 +5,10 @@ odd_length([_|Tail]) :- even_length(Tail).
 
 is_even(List) :- even_length(List).
 is_odd(List) :- odd_length(List).
+
+
+Output:-
+?-is_even([a,b,c,d]).
+true.
+?-is_odd([a,b,c,d,e)].
+true.
